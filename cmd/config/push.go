563a88442d2319280d72391bfb1d1c1e58d38b56
Loïc Kami <lkami@bogial.com>
2023-09-29 01:34:56 +0200

    Update config push command to save env variables in database.

--- cmd/config/push.go
+++ cmd/config/push.go
@@ -3,23 +3,169 @@ package config
 import (
 	"fmt"
 
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/kamicodaxe/envzy-cli/internal/api"
+	"github.com/kamicodaxe/envzy-cli/internal/app"
+	"github.com/kamicodaxe/envzy-cli/internal/constants"
+	"github.com/kamicodaxe/envzy-cli/internal/models"
+	"github.com/kamicodaxe/envzy-cli/utils/dotenv"
 	"github.com/spf13/cobra"
 )
 
+var pushProject string
 var pushBranch string
 
 var pushCmd = &cobra.Command{
-	Use:   "push [branch]",
-	Short: "Push committed changes to a specific branch or configuration",
+	Use:   "push [path/to/dotenv]",
+	Short: "Pushes all env files variables in env-file to envy",
 	Args:  cobra.ExactArgs(1), // Requires exactly one argument
 	Run: func(cmd *cobra.Command, args []string) {
-		branchName := args[0]
-		fmt.Printf("Pushing changes to branch: %s\n", branchName)
-		// Logic here
-		fmt.Println("Changes pushed successfully.")
+		var selectedProject *models.Project
+		var selectedBranch *models.Branch
+		envFile := args[0]
+		fmt.Printf("Adding environment file: %s\n", envFile)
+
+		if pushProject == "" {
+			activeProject := api.GetCurrentProject()
+
+			if activeProject != nil {
+				pushProject = activeProject.Name
+				selectedProject = activeProject
+			} else {
+				selectedProject = SelectProject()
+				if selectedProject != nil {
+					pushProject = selectedProject.Name
+				} else {
+					fmt.Println("No Project selected")
+					return
+				}
+			}
+		}
+
+		if pushBranch == "" {
+			activeBranch := api.GetCurrentBranch()
+
+			if activeBranch != nil {
+				pushBranch = activeBranch.Name
+				selectedBranch = activeBranch
+			} else {
+				selectedBranch = SelectBranch(selectedProject.ID)
+				if selectedBranch != nil {
+					pushBranch = selectedBranch.Name
+				} else {
+					fmt.Println("No Branch selected")
+					return
+				}
+			}
+
+		}
+
+		fmt.Printf("Project: %s\n", pushProject)
+		fmt.Printf("Branch: %s\n", pushBranch)
+
+		// Add dotenv file to secrets
+		err := dotenv.AddDotenvFileToSecrets(envFile, *selectedProject, *selectedBranch)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
 	},
+}
+
+// SelectProject prompts the user to select a project from available options.
+// It returns the selected project object.
+func SelectProject() *models.Project {
+	// Retrieve the list of available projects from your database
+	var selectedProject *models.Project
+	var selectedProjectName string
+
+	projects, err := api.GetProjects()
+	if err != nil {
+		fmt.Println("An error occurred.")
+		return nil
+	}
+
+	if len(projects) == 0 {
+		fmt.Println("No projects available.")
+		return nil
+	}
+
+	// Define a slice of project names for the survey prompt
+	projectNames := make([]string, len(projects))
+	for i, project := range projects {
+		projectNames[i] = project.Name
+	}
+
+	prompt := &survey.Select{
+		Message: "Select a project:",
+		Options: projectNames,
+	}
+
+	// Prompt the user to select a project
+	survey.AskOne(prompt, &selectedProjectName)
+
+	for _, project := range projects {
+		if project.Name == selectedProjectName {
+			selectedProject = &project
+		}
+	}
+
+	// Update the application's state with the selected project
+	kvstore := app.GetKVStore()
+
+	// TODO: Handle errors
+	kvstore.SetString(constants.PROJECT_NAME, selectedProject.Name)
+	kvstore.SetUInt(constants.PROJECT_ID, selectedProject.ID)
+
+	name := kvstore.String(constants.PROJECT_NAME)
+	fmt.Printf("Project selected: %s\n", name)
+
+	return selectedProject
+}
+
+// SelectBranch prompts the user to select a branch from available options for a given project.
+// It returns the selected branch object.
+func SelectBranch(projectID uint) *models.Branch {
+	// Retrieve the list of branches for the specified project from the API
+	branches, err := api.GetBranchesByProjectID(projectID)
+	if err != nil {
+		fmt.Println("An error occurred while retrieving branches.")
+		return nil
+	}
+
+	if len(branches) == 0 {
+		fmt.Println("No branches available for the selected project.")
+		return nil
+	}
+
+	// Define a slice of branch names for the survey prompt
+	branchNames := make([]string, len(branches))
+	for i, branch := range branches {
+		branchNames[i] = branch.Name
+	}
+
+	prompt := &survey.Select{
+		Message: "Select a branch:",
+		Options: branchNames,
+	}
+
+	var selectedBranchName string
+	survey.AskOne(prompt, &selectedBranchName)
+
+	for _, branch := range branches {
+		if branch.Name == selectedBranchName {
+			return &branch
+		}
+	}
+
+	// In case no branch is found (this should not happen)
+	return nil
 }
 
 func init() {
 	// Add flags or additional configuration for the "push" command here, if needed
+	// TODO: Uncomment the flags bellow and use it to enable the flexibility to specify a project and branch
+	// pushCmd.PersistentFlags().StringVarP(&pushProject, "project", "p", "", "Project name")
+	// pushCmd.PersistentFlags().StringVarP(&pushBranch, "branch", "b", "", "Branch name")
 }
--- utils/dotenv/dotenv.go
+++ utils/dotenv/dotenv.go
@@ -3,6 +3,9 @@ package dotenv
 import (
 	"fmt"
 	"os"
+
+	"github.com/kamicodaxe/envzy-cli/internal/app"
+	"github.com/kamicodaxe/envzy-cli/internal/models"
 )
 
 // Variable represents a dotenv variable and its associated comment.
@@ -22,15 +25,36 @@ func ReadDotenvFile(dotenvFilePath string) (string, error) {
 }
 
 // StoreVariablesInDatabase stores the extracted variables in the secrets table.
-func StoreVariablesInDatabase(variables []Variable) {
+func StoreVariablesInDatabase(variables []Variable, project models.Project, branch models.Branch) {
+	db := app.GetDB()
+	// Iterate through the extracted variables
 	for _, variable := range variables {
-		// Replace with your logic to store in the secrets table
-		fmt.Printf("Storing in secrets table: Key=%s, Value=%s, Comment=%s\n", variable.Key, variable.Value, variable.Comment)
+		// Check if the secret already exists in the database
+		var existingSecret models.Secret
+		result := db.Where("project_id = ? AND branch_id = ? AND name = ?", project.ID, branch.ID, variable.Key).First(&existingSecret)
+		if result.Error != nil {
+			// Secret doesn't exist, so create a new one
+			newSecret := models.Secret{
+				ProjectID:   project.ID,
+				BranchID:    branch.ID,
+				Name:        variable.Key,
+				Value:       variable.Value,
+				Description: variable.Comment,
+			}
+			db.Create(&newSecret)
+			fmt.Printf("Added new secret: Key=%s, Value=%s, Comment=%s\n", variable.Key, variable.Value, variable.Comment)
+		} else {
+			// Secret already exists, update it
+			existingSecret.Value = variable.Value
+			existingSecret.Description = variable.Comment
+			db.Save(&existingSecret)
+			fmt.Printf("Updated secret: Key=%s, Value=%s, Comment=%s\n", variable.Key, variable.Value, variable.Comment)
+		}
 	}
 }
 
 // AddDotenvFileToSecrets orchestrates the process.
-func AddDotenvFileToSecrets(dotenvFilePath string) error {
+func AddDotenvFileToSecrets(dotenvFilePath string, project models.Project, branch models.Branch) error {
 	// Step 1: Read the dotenv file
 	content, err := ReadDotenvFile(dotenvFilePath)
 	if err != nil {
@@ -41,7 +65,7 @@ func AddDotenvFileToSecrets(dotenvFilePath string) error {
 	variables := ParseDotenvFile(content) // From env_parser.go
 
 	// Step 3: Store variables in the secrets table
-	StoreVariablesInDatabase(variables)
+	StoreVariablesInDatabase(variables, project, branch)
 
 	return nil
 }
