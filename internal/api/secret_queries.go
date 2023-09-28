05e11b818d13fbc2c81f95ef2d4ad3d46ddfc817
Loïc Kami <lkami@bogial.com>
2023-09-29 01:48:18 +0200

    Update secret_queries loading db from app.GetDB() Rename GetAllSecretsByBranch to GetSecretsByProjectAndBranch

--- internal/api/secret_queries.go
+++ internal/api/secret_queries.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/kamicodaxe/envzy-cli/internal/app"
 	"github.com/kamicodaxe/envzy-cli/internal/models"
 	"gorm.io/gorm"
 )
@@ -15,7 +16,8 @@ func GetSecretByID(db *gorm.DB, secretID uint) (*models.Secret, error) {
 }
 
 // GetSecretByName retrieves a secret by its name within a project.
-func GetSecretByName(db *gorm.DB, projectID uint, secretName string) (*models.Secret, error) {
+func GetSecretByName(projectID uint, secretName string) (*models.Secret, error) {
+	db := app.GetDB()
 	var secret models.Secret
 	if err := db.Where("project_id = ? AND name = ?", projectID, secretName).First(&secret).Error; err != nil {
 		return nil, err
@@ -24,25 +26,29 @@ func GetSecretByName(db *gorm.DB, projectID uint, secretName string) (*models.Se
 }
 
 // CreateSecret creates a new secret in the database.
-func CreateSecret(db *gorm.DB, secret *models.Secret) error {
+func CreateSecret(secret *models.Secret) error {
+	db := app.GetDB()
 	return db.Create(secret).Error
 }
 
 // UpdateSecret updates an existing secret in the database.
-func UpdateSecret(db *gorm.DB, secret *models.Secret) error {
+func UpdateSecret(secret *models.Secret) error {
+	db := app.GetDB()
 	return db.Save(secret).Error
 }
 
 // DeleteSecretByID deletes a secret by its ID.
-func DeleteSecretByID(db *gorm.DB, secretID uint) error {
+func DeleteSecretByID(secretID uint) error {
+	db := app.GetDB()
 	return db.Delete(&models.Secret{}, secretID).Error
 }
 
-// GetAllSecretsByBranch retrieves all secrets within a specific branch of a project.
-func GetAllSecretsByBranch(db *gorm.DB, projectID uint, branchName string) ([]models.Secret, error) {
+// GetSecretsByProjectAndBranch retrieves all secrets within a specific branch of a project.
+func GetSecretsByProjectAndBranch(projectID uint, branchID uint) ([]models.Secret, error) {
+	db := app.GetDB()
 	var secrets []models.Secret
 
-	if err := db.Where("project_id = ? AND branch = ?", projectID, branchName).Find(&secrets).Error; err != nil {
+	if err := db.Where("project_id = ? AND branch_id = ?", projectID, branchID).Find(&secrets).Error; err != nil {
 		return nil, err
 	}
 
