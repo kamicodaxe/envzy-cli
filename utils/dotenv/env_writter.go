a80a8a495af4c376a05d05956bb315e7fec6f432
Loïc Kami <lkami@bogial.com>
2023-09-29 13:28:32 +0200

    Update env_writter to delete a file if it exist before writing env variables.

--- utils/dotenv/env_writter.go
+++ utils/dotenv/env_writter.go
@@ -6,6 +6,14 @@ import (
 )
 
 func WriteToEnvFile(filename string, key, value string, comment string) error {
+	// Delete the file if it exists
+	if _, err := os.Stat(filename); err == nil {
+		err := os.Remove(filename)
+		if err != nil {
+			return err
+		}
+	}
+
 	// Open the .env file in append mode
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
